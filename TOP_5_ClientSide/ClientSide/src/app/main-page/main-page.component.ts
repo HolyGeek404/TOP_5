9c9161be681c14bafba17a68d65450ccb2c02c91
HolyGeek404 <wiktorzme@gmail.com>
2019-10-21 20:14:10 +0200

    Create basic design & logic of main page

--- TOP_5_ClientSide/ClientSide/src/app/main-page/main-page.component.ts
+++ TOP_5_ClientSide/ClientSide/src/app/main-page/main-page.component.ts
@@ -11,6 +11,7 @@ export class MainPageComponent implements OnInit {
   constructor(private http: HttpService) { }
   
   result: Object;
+  activeGame: boolean = true;
 
   ngOnInit() 
   {
@@ -21,4 +22,20 @@ export class MainPageComponent implements OnInit {
       });
   }
 
+  randomColor()
+  {
+    var length = 6;
+    var chars = '0123456789ABCDEF';
+    var hex = '#';
+
+    while(length--)
+    {
+      hex += chars[(Math.random() * 16) | 0];
+    }
+
+    return hex;
+  }
+
+  //TODO: dodanie funkcji ktora iteruje po tablicy kategorii - funkcja z parametrem zmiennej 
+  //      div'a w ktorym jest wywplywana
 }
