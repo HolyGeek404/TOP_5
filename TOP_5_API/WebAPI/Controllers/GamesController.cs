6e45cc8cb88ab2b943a479be124bba385c1c9d6e
HolyGeek404 <wiktorzme@gmail.com>
2020-01-29 00:32:50 +0100

    Creating layout for GameComponent & refresh API project

---
+++ TOP_5_API/WebAPI/Controllers/GamesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> ReturnAllCategories()
+        {
+            return new string[] { "games", "films", "actors" };
+        }
+
+        [HttpGet("{categoryName}")]
+        public ActionResult<string> ReturnSpecificCategory(string categoryName)
+        {
+            switch (categoryName)
+            {
+                case "games":
+                    return "gamesaaaa";
+
+                case "films":
+                    return "filmsaaaa";
+
+                case "actors":
+                    return "actorsaaa";
+
+                default:
+                    return "category doesn't exist";
+
+            }
+        }
+    }
+}
