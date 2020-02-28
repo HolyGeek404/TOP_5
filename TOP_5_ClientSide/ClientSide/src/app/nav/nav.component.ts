2ede52c06ccdc8077eacf3a1dc9431f96b68293a
HolyGeek 404 <wiktorzme@gmail.com>
2020-02-28 21:07:51 +0100

    Create additional div in nav.component for describe category elements

--- TOP_5_ClientSide/ClientSide/src/app/nav/nav.component.ts
+++ TOP_5_ClientSide/ClientSide/src/app/nav/nav.component.ts
@@ -10,6 +10,23 @@ export class NavComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  ShowSpecificCategoryElements()
+  {
+    $("#category_elements_description").animate({ height: 160});
+    $("#elements_list").animate({ height: "85%"});
+    $("#elements_description").fadeIn(300);
+    $("#collapse").animate({ height: "15%"});
+  }
+
+  HideSpecificCategoryElements()
+  {
+    $("#category_elements_description").animate({ height: 0});
+    $("#elements_list").animate({ height: "0%"});
+    $("#elements_description").fadeOut(300);
+    $("#collapse").animate({ height: "0%"});
   }
 
 }
